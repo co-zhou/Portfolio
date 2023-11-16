61ef31a5da00f82ad58889e8bd3430658fcc4617
unknown <coreyzh@gmail.com>
2023-11-16 14:02:49 -0800

    script and package

--- ecommerce/server-script.sh
+++ ecommerce/server-script.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-sudo yum update
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-echo "<h1>Hello from Terraform</h1>" | sudo tee /var/www/html/index.html
+sudo yum update -y
+sudo yum install -y git
+git clone --depth 1 --no-checkout https://github.com/co-zhou/Portfolio.git
+cd Portfolio
+git sparse-checkout set ecommerce
+git checkout
+cd ecommerce
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+
+npx npm-check-updates -u -y
+npm install
+npm install -g serve
+
+npm run start
+
